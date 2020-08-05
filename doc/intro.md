86a129970ec37e73fd50db007803afd6914ba8e4
Branko Skoric <b.skoric@netconomy.net>
2020-08-05 16:32:57 +0200

    Repo init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simple-tweet
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/simple_tweet/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-tweet.core-test
+  (:require [clojure.test :refer :all]
+            [simple-tweet.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
